c5c67804988509db54baab74faf6b39c5a3ba35b
Iuliia1991 <106701628+Iuliia1991@users.noreply.github.com>
2023-04-20 14:15:24 +0700

    Update test_items.py
    Bug fixing

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_page_contains_add_to_basket_button(browser):
     browser.get(link)
-    time.sleep(30)
+    #time.sleep(30)
     assert len(browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')) > 0
